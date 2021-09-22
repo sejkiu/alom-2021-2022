a94d73f177a675252742b8a66af551ccc7d102fe
Julien WITTOUCK <julien.wittouck@gmail.com>
2021-09-22 06:25:03 +0200

    ♻️ : update PassengerService bad example

--- cours/03-spring/carbon/passengerService.java
+++ cours/03-spring/carbon/passengerService.java
@@ -1,19 +1,19 @@
+import java.sql.Driver;mport java.sql.Driver;
+
 class PassengerService {
     private PaymentService paymentService = new PaymentService();
     private NotificationService notificationService = new NotificationService();
     void pay(Trip trip){
         this.paymentService.pay(trip.getPrice(), trip.getDriver());
         var event = new TripPaidEvent(trip.getDriver());
-        this.notificationService.notify(event);
+        this.notify(event);
+    }
+    void notify(Event event){
+        [...]
     }
 }
 class PaymentService {
     void pay(float price, Driver driver){
-        [...]
-    }
-}
-class NotificationService {
-    void notify(Event event){
         [...]
     }
 }
